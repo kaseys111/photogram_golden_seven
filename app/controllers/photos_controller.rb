7719992870eaa80371d4610e403361abc12643d1
Kasey Stonehill <kaseys111@users.noreply.github.com>
2016-10-30 18:56:31 -0500

    READ and CREATE complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,36 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
 
 
+  def show
+    id = params["id"]
+
+    i = Photo.find(id)
+
+    @img_src = i.source
+
+    @desc = i.caption
+
+  end
+
+
+  def new_form
+    render ("/photos/new_form.html.erb")
+  end
+
+
+  def create
+    p = Photo.new
+    p.source = params["url"]
+    p.caption = params["caption"]
+
+    if p.source != nil
+      p.save
+    end
+
+    redirect_to("http://localhost:3000")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "photos", :action => "index" })
+  get("/",              { :controller => "photos", :action => "index" })
+
+  # Routes to CREATE photos
+  get("/photos/new",    { :controller => "photos", :action => "new_form" })
+
+  get("/create", { :controller => "photos", :action => "create" })
 
   # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos",        { :controller => "photos", :action => "index" })
+
+  get("/photos/:id",    { :controller => "photos", :action => "show" })
+
 
 end
